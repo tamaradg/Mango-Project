2cb7c5096e1ea74367411eefa42d8aabb757fcb2
Tamara de Gaetano <tamara.degaetano@yahoo.com>
2022-09-13 12:43:56 +0200

    finish creating user signup route

--- src/users.router.js
+++ src/users.router.js
@@ -1,6 +1,7 @@
 import User from "./user.model.js";
 import express from "express";
 import bcrypt from "bcrypt";
+import _ from "lodash";
 const usersRouter = express.Router();
 
 // Create user
@@ -13,6 +14,13 @@ usersRouter.post("/signup", async (req, res) => {
       .status(400)
       .send(`User with email ${req.body.email} already exists.`);
   }
+  // ici on créé le user
+  const newUser = new User(req.body);
+  // ici on hash et on salt(2eme argument) le password
+  newUser.password = await bcrypt.hash(req.body.password, 10);
+  // on sauvegarde en db
+  await newUser.save();
+  return res.status(201).send(_.pick(newUser, ["name", "email"]));
 });
 
 
